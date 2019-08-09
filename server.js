a6b4c84583803b28147531f97d96d7a21b5df8c1
Murphy <jim.murphy@nera.com>
2019-08-09 11:58:08 -0400

    update server

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const app = express();
 
 app.use(express.static('./dist/client'));
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/dist/client/index.html/'));
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '/dist/client/index.html/'));
 });
+
+app.get('*', function(req, res) {
+  res.status(404).sendFile(__dirname, '/dist/client/index.html/');
+})
 
 app.listen(process.env.PORT || 8080);
