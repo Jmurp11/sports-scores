c99ee9c1133dabe57223c965fc324496ef06043c
Murphy <jim.murphy@nera.com>
2019-08-09 09:33:10 -0400

    change server.js paths

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sports-scores'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname,'/dist/sports-scores/index.html'));
+res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
